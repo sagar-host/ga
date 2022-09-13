7a22fbd5ced2d102b4d1852e10c69e8c06c5416a
sagar <92850073+sagar-host@users.noreply.github.com>
2022-09-13 21:36:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useEffect} from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./Components/Home/home"
